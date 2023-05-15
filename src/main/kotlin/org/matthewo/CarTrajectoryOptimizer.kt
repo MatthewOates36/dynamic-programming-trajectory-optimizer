d8d262eae7f8a290deceff9df9f59483c42ea58b
Matthew Oates <uni.oates@gmail.com>
2023-05-15 13:43:10 -0600

    time-optimal

--- src/main/kotlin/org/matthewo/CarTrajectoryOptimizer.kt
+++ src/main/kotlin/org/matthewo/CarTrajectoryOptimizer.kt
@@ -5,7 +5,7 @@ import kotlin.math.abs
 
 class CarTrajectoryOptimizer {
 
-    val table = TrajectoryTable(-90.0, 90.0, 1.0, -1.0, 1.0, 0.002, Pair(1000.0, 0.0))
+    val table = TrajectoryTable(-90.0, 90.0, 1.0, -1.0, 1.0, 0.002, Pair(10000.0, 0.0))
 
     fun optimize(): TrajectoryTable {
         table[0.0, 0.0] = Pair(0.0, 0.0)
@@ -19,7 +19,7 @@ class CarTrajectoryOptimizer {
                     val action = it.toDouble()
                     val update = state.update(action, 1.0, 0.05)
                     val (cost, _) = table[update.heading, update.y]
-                    val updatedCost = cost + abs(update.y) + abs(update.heading) / 90.0
+                    val updatedCost = cost + 1.0
                     if (updatedCost >= currentCost) {
                         current
                     } else {
